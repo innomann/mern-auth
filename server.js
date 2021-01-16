07b6d557501fd1dd14f05f4677ea6ea33ddc516e
Birahinduka Steven <birahindukasteven@Birahindukas-MacBook-Air.local>
2021-01-16 16:41:32 +0100

    version 1.0.0

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require ('express')
 const mongoose = require ('mongoose')
 const bodyParser = require ('body-parser')
 const Cors = require ('cors')
+const path = require('path')
 const passport = require ('passport')
 const app = express()
 
